5aedb09c95b98a0378694322a22f934efaf689e8
HarrisonFld <fieldharrison@proton.me>
2025-02-21 20:18:37 -0500

    Initialized maven repository

---
+++ src/main/java/org/wolfsonrobotics/RobotWebServer/App.java
@@ -0,0 +1,13 @@
+package org.wolfsonrobotics.RobotWebServer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
