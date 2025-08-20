2849659300b014f4a1b564d86ac7f0be9e1a467f
HarrisonFld <fieldharrison@proton.me>
2025-08-20 19:46:05 -0400

    Modified SeverConfig to be Mutable

--- lib/src/main/java/org/wolfsonrobotics/RobotWebServer/ServerConfig.java
+++ lib/src/main/java/org/wolfsonrobotics/RobotWebServer/ServerConfig.java
@@ -31,36 +31,36 @@ import java.util.Map;
 
 public class ServerConfig {
 
-    public static final String WEBROOT = Environment.getExternalStorageDirectory().getPath() + "/website/";
-    public static final int PORT = 39537;
-    public static final int SOCKET_START_TIMEOUT = 60000;
+    public static String WEBROOT = Environment.getExternalStorageDirectory().getPath() + "/website/";
+    public static int PORT = 39537;
+    public static int SOCKET_START_TIMEOUT = 60000;
 
-    public static final String CONTROL_HUB_STORAGE = Environment.getExternalStorageDirectory().getPath();
-    public static final String[] COMM_METHODS = null;
-    public static final String[] EXCLUDED_COMM_METHODS = { "getExcludedMethods", "getCameraFeed", "runOpMode", "idle", "init", "init_loop", "isStarted", "isStopRequested", "loop", "opModeInInit", "opModeIsActive", "start", "stop", "waitForStart", "getRuntime", "internalPostInitLoop", "internalPostLoop", "internalPreInit", "internalUpdateTelemetryNow", "resetRuntime", "terminateOpModeNow", "updateTelemetry", "requestOpModeStop", "clone", "equals", "finalize", "getClass", "hashCode", "notify", "notifyAll", "toString" };
+    public static String CONTROL_HUB_STORAGE = Environment.getExternalStorageDirectory().getPath();
+    public static String[] COMM_METHODS = null;
+    public static String[] EXCLUDED_COMM_METHODS = { "getExcludedMethods", "getCameraFeed", "runOpMode", "idle", "init", "init_loop", "isStarted", "isStopRequested", "loop", "opModeInInit", "opModeIsActive", "start", "stop", "waitForStart", "getRuntime", "internalPostInitLoop", "internalPostLoop", "internalPreInit", "internalUpdateTelemetryNow", "resetRuntime", "terminateOpModeNow", "updateTelemetry", "requestOpModeStop", "clone", "equals", "finalize", "getClass", "hashCode", "notify", "notifyAll", "toString" };
 
-    public static final int DEFAULT_SOCKET_MSG_FREQUENCY = 100;
+    public static int DEFAULT_SOCKET_MSG_FREQUENCY = 100;
 
     // Hardcoded mime types, as no Android packages (even the NanoHTTPD version) serve
     // these particular mime types correctly
-    public static final Map<String, String> mimeTypes = Collections.unmodifiableMap(new HashMap<String, String>() {{
+    public static Map<String, String> mimeTypes = Collections.unmodifiableMap(new HashMap<String, String>() {{
         put("js", "text/javascript");
     }});
 
-    public static final Map<String, Class<? extends RobotAPI>> robotAPIMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends RobotAPI>>() {{
+    public static Map<String, Class<? extends RobotAPI>> robotAPIMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends RobotAPI>>() {{
         put("/robot/all_methods", AllMethods.class);
         put("/robot/call_method", CallMethod.class);
         put("/robot/all_fields", AllFields.class);
         put("/robot/camera_feed", CameraFeed.class);
     }});
 
-    public static final Map<String, Class<? extends BaseSocket>> socketMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends BaseSocket>>() {{
+    public static Map<String, Class<? extends BaseSocket>> socketMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends BaseSocket>>() {{
         put("robot/camera_feed", CameraSocket.class);
         put("robot/device_info", DeviceInfoSocket.class);
         put("robot/telemetry", TelemetrySocket.class);
     }});
 
-    public static final Map<String, Class<? extends FileAPI>> fileAPIMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends FileAPI>>() {{
+    public static Map<String, Class<? extends FileAPI>> fileAPIMap = Collections.unmodifiableMap(new HashMap<String, Class<? extends FileAPI>>() {{
         put("/file/listing", Listing.class);
         put("/file/file_operation", FileAction.class);
         put("/file/directory_operation", DirectoryAction.class);
@@ -68,15 +68,15 @@ public class ServerConfig {
 
 
 
-    public static final String CAMERA_FEED_METHOD = "getCameraFeed";
-    public static final Map<String, String> motorProperties = Collections.unmodifiableMap(new HashMap<String, String>() {{
+    public static String CAMERA_FEED_METHOD = "getCameraFeed";
+    public static Map<String, String> motorProperties = Collections.unmodifiableMap(new HashMap<String, String>() {{
         put("Power", "getPower");
         put("Position", "getCurrentPosition");
     }});
-    public static final Map<String, String> servoProperties = Collections.unmodifiableMap(new HashMap<String, String>() {{
+    public static Map<String, String> servoProperties = Collections.unmodifiableMap(new HashMap<String, String>() {{
         put("Position", "getPosition");
     }});
-    public static final Map<Class<? extends HardwareDevice>, Map<String, String>> deviceInfoMap = Collections.unmodifiableMap(new HashMap<Class<? extends HardwareDevice>, Map<String, String>>() {{
+    public static Map<Class<? extends HardwareDevice>, Map<String, String>> deviceInfoMap = Collections.unmodifiableMap(new HashMap<Class<? extends HardwareDevice>, Map<String, String>>() {{
         put(DcMotor.class, motorProperties);
         put(DcMotorEx.class, motorProperties);
         put(DcMotorImpl.class, motorProperties);
@@ -86,6 +86,6 @@ public class ServerConfig {
         put(ServoImplEx.class, servoProperties);
     }});
 
-    public static final String TELEMETRY_FIELD = "telemetry";
+    public static String TELEMETRY_FIELD = "telemetry";
 
 }
